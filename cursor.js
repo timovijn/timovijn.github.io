1765edcaaa5c94145e8496e1e58c28ad45fc3e5f
Timo Vijn <timovijn@gmail.com>
2021-10-27 16:39:35 +0200

    Update

---
+++ cursor.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function() {
+
+    var mouseX = 0, mouseY = 0;
+    var xp = 0, yp = 0;
+     
+    $(document).mousemove(function(e){
+      mouseX = e.pageX - 7;
+      mouseY = e.pageY - 7;
+    });
+      
+    setInterval(function(){
+      xp += ((mouseX - xp)/40);
+      yp += ((mouseY - yp)/40);
+      $("#circle").css({left: xp +'px', top: yp +'px'});
+    }, 5);
+  
+  });
