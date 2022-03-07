3eb4d4d877ca18a7cad1f691c622f405835e7ae3
timovijn <timovijn@gmail.com>
2022-03-07 18:55:59 +0100

    Update

--- cursor.js
+++ cursor.js
@@ -4,8 +4,8 @@ jQuery(document).ready(function() {
     var xp = 0, yp = 0;
      
     $(document).mousemove(function(e){
-      mouseX = e.pageX - 15;
-      mouseY = e.pageY - 15;
+      mouseX = e.pageX - 30;
+      mouseY = e.pageY - 30;
     });
       
     setInterval(function(){
@@ -14,4 +14,31 @@ jQuery(document).ready(function() {
       $("#circle").css({left: xp +'px', top: yp +'px'});
     }, 5);
   
+  });
+
+  // $(".welcome-image-image").mouseenter(function(){
+  //   $(".circle").css("width","60px");
+  //  });
+ 
+  // $(".welcome-image-image").mouseleave(function(){
+  //   $(".circle").css("width","30px").animate();
+  //  });
+
+  // $(".welcome-image-image").hover($(".circle").css("width","60px"), $(".circle").css("width","30px"))
+
+  var cursor = $(".circle");
+
+  $(".o-h5")
+  .mouseenter(function() {
+      cursor.css({
+          // width: "60px"
+          // transform: "rotate(1deg)",
+          transform: "scale(1)"
+      });
+  })
+  .mouseleave(function() {
+      cursor.css({
+          // width: "30px"
+          transform: "scale(0.5)"
+      });
   });
